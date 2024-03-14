628c0386eee79d7b64d4d31fd94ae45d9e40c1e2
tsokac2 <tsokac2@gmail.com>
2024-03-14 12:36:13 +0000

    Added function part of JavaScript Refresher

--- code/scripts/app.js
+++ code/scripts/app.js
@@ -1,1 +1,15 @@
 // console.log("Hello World");
+
+// import { userValidationHandler } from "./util.js"; // importing userValidationHandler function from "./util.js"
+// const userAge = () => {
+//     try {
+//         userValidationHandler(55);
+//         userValidationHandler("Tom");
+//     } catch(e) {
+//         console.log("Error: ", e.message);
+//     }
+// };
+
+// userAge();
+
+
---
+++ code/scripts/util.js
@@ -0,0 +1,10 @@
+// Exporting userValidationHandler function
+// export const userValidationHandler = age => {
+//     if(typeof age !== "number"){
+//         throw new Error(`User "${age} is not a number value!`);
+//     } else {
+//         console.log(`User age is: ${age}!`);
+//     }
+// };
+
+
